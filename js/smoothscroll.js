121e32c0cbc4d7124920f64b99aa9691988e9b93
Amit Kushwaha <amit_kushwaha@outlook.com>
2016-10-24 00:36:23 +0530

    added animation in home page

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -15,7 +15,33 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
  * @author Ariel Flesler
  * @version 1.4.3.1
  */
-;(function ($) {
+$(document).ready(function () {
+    $('body').show();
+    $("#js-rotating_2").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "flipInX",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 2000
+    });
+});
+
+
+$(document).ready(function () {
+    $('body').show();
+    $("#js-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "flipInX",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 2500
+    });
+});
+
+
+(function ($) {
     var h = $.scrollTo = function (a, b, c) {
         $(window).scrollTo(a, b, c)
     };
